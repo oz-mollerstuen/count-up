0b33d2b86aca7d25aa9433aadbf750234f766a0a
oz-mollerstuen <mollerstuen.works@gmail.com>
2022-10-26 12:00:16 -0700

    add index files for input and submit, js for loop, and bootstrap
    Co-authored-by: anahi ruiz <anahi.sulikey@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", function() {
+
+    const form = document.getElementById("count");
+    form.addEventListener("submit", countDracula);
+
+  });
+
+  //-------------------------------------------------------------
+     function countDracula(event) {
+    event.preventDefault();
+  
+    const num1 = parseInt(document.querySelector("input#input1").value);
+    const num2 = parseInt(document.querySelector("input#input2").value);
+
+if (num2 > num1) {
+    document.getElementById("output").innerText = ("Please make 'count' larger than 'multiples of'")
+};
+else if (num1 === isNaN() || num2 === isNaN()) {
+document.getElementById("output").innerText = ("Please only use real numbers'")
+     };
+
+    // let newArray = []
+    // for (let index = 0; index <= num1; index += num2)
+    // {newArray.push([index])
+    //  }
+    //  document.getElementById("output").innerText = (newArray)
+    }
