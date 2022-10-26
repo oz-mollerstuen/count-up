b91addc090ae9d0d7a20114242b28260370ba38a
oz-mollerstuen <mollerstuen.works@gmail.com>
2022-10-26 13:11:50 -0700

    add if else statements for output properties, add bootstrap for style
    Co-authored-by: anahi ruiz <anahi.sulikey@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,23 @@ window.addEventListener("load", function() {
 
 if (num2 > num1) {
     document.getElementById("output").innerText = ("Please make 'count' larger than 'multiples of'")
-};
-else if (num1 === isNaN() || num2 === isNaN()) {
-document.getElementById("output").innerText = ("Please only use real numbers'")
-     };
+}
 
-    // let newArray = []
-    // for (let index = 0; index <= num1; index += num2)
-    // {newArray.push([index])
-    //  }
-    //  document.getElementById("output").innerText = (newArray)
+else if (num1 === isNaN() || num2 === isNaN()) {
+document.getElementById("output").innerText = ("Please only use real numbers")
+}
+
+else if (num1 < 1 || num2 < 1) {
+document.getElementById("output").innerText = ("Please only use numbers of value '1' or more");
+}
+
+
+else {
+
+    let newArray = []
+    for (let index = 0; index <= num1; index += num2)
+    {newArray.push([index])
     }
+    document.getElementById("output").innerText = (newArray)
+}
+     }
